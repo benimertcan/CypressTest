dfd306c89e16f777d3ca463cb151e0054b363872
Mertcan Kurt <mertcankurt25@gmail.com>
2024-11-02 02:16:34 +0300

    testing email and password components to be work like excepted

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -7,4 +7,15 @@ describe('Hatalı durumlarda beklenen hata mesajları görünüyor ve buton disa
       cy.contains("Please enter a valid email address")
       cy.get('[data-cy="buttonInput"]').should('be.disabled');
     })
+    it('Email ve Password yanlış', () => {
+        cy.visit('http://localhost:5173/')
+        cy.get('[data-cy="emailInput"]').type("aaaa")
+        cy.get('[data-cy="passwordInput"]').type("aaa")
+    
+        cy.get('[data-cy="errorMessageEmail"]').should('exist');
+        cy.get('[data-cy="errorMessagePassword"]').should('exist');
+        cy.contains("Please enter a valid email address")
+        cy.contains("Password must be at least 4 characters long")
+        cy.get('[data-cy="buttonInput"]').should('be.disabled');
+      })
   })
