45ac1b294dd349e558679c3c9186619d0d414c6e
Mertcan Kurt <mertcankurt25@gmail.com>
2024-11-02 02:17:24 +0300

    testing all components to be work like excepted

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -18,4 +18,12 @@ describe('Hatalı durumlarda beklenen hata mesajları görünüyor ve buton disa
         cy.contains("Password must be at least 4 characters long")
         cy.get('[data-cy="buttonInput"]').should('be.disabled');
       })
+      it('Email ve Password doğru ama terms kabul edilmedi', () => {
+        cy.visit('http://localhost:5173/')
+        cy.get('[data-cy="emailInput"]').type("aaaa@gmail.com")
+        cy.get('[data-cy="passwordInput"]').type("aaaaa")
+        cy.get('[data-cy="termsInput"]').not("check")
+    
+        cy.get('[data-cy="buttonInput"]').should('be.disabled');
+      })
   })
